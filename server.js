c7a3cc47786d69e29e2c55d97ef90760e6909f8f
dtoney12 <dtoney12@gmail.com>
2017-03-19 22:03:11 -0700

    calendar page now loads from login page, db connected

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const express = require('express');
 const SocketServer = require('ws').Server;
 const path = require('path');
 const bodyParser = require('body-parser');
+const db = require('./database/db.js')
 
 const PORT = process.env.PORT || 3000;
 const CLIENT_FILES = path.join(__dirname, './public');
@@ -22,12 +23,18 @@ WSserver.on('connection', (client) => {
 	var clientID = client.upgradeReq.rawHeaders[21].slice(0,5);
 	console.log('\n' + clientID + ' <---- connected');
 	var sendObj = {};
+	sendObj.user = {};
+	sendObj.user.loginOk = false;
 	sendObj.time = new Date().toTimeString();
 	client.send( JSON.stringify(sendObj) );
 
 	client.on('message', (recObj)=> {
 		recObj = JSON.parse(recObj);
-		console.log('\n' + clientID + ' attempting to update ')
+		console.log('\n' + clientID + ' attempting to update ');
+		if (recObj.username.length > 0 && recObj.password.length > 0) {
+			sendObj.user.loginOk = true;
+			client.send( JSON.stringify(sendObj) );
+		}
 		});
 
 	client.on('close', ()=> {
