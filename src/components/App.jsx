a7b39038c2b3ddae3e68b0063eae78eacd494b18
Tim Nguyen <tungnh91@gmail.com>
2017-03-20 15:36:18 -0700

    insert form is now available

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,25 @@
+'use strict';
 import React from 'react';
 import Month from './Month.jsx';
 import example from '../ExampleData.js';
 import $ from 'jquery';
+import t from 'tcomb-form';
 
+//easier way to handle form without writing a whole bunch of html
+const Form = t.form.Form;
+const FormSchema = t.struct({
+  name: t.Str,
+  startTime: t.Str,
+  endTime: t.Str,
+  manager: t.Num,
+  completed: t.Bool,
+})
 class App extends React.Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			events: []
+			events: [],
+			name: null
 		}
 	}
 
@@ -16,6 +28,14 @@ class App extends React.Component {
 			events: example
 		});
 	}
+
+  onSubmit(evt) {
+    evt.preventDefault()
+    const value = this.refs.form.getValue()
+    if (value) {
+      console.log(value)
+    }
+  }
 
 	render() {
 	  return (
@@ -28,6 +48,15 @@ class App extends React.Component {
 	  	  <div className="days">Saturday</div>
 	  	  <div className="days">Sunday</div>
 		  	<div><Month month={this.state.events}/></div>
+			  	<div className = "userInputForm">
+			  	<h1>Create an event</h1>
+	        <form onSubmit={this.onSubmit}>
+	        <t.form.Form ref="form" type={FormSchema} />
+	        <div className="form-group">
+	          <button type="submit" className="btn btn-primary">Save</button>
+	        </div>
+		      </form>
+		      </div>
 		  </div>	
 	  );
 	}
