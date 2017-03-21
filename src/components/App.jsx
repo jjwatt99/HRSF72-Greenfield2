1cb4521a9b9baf4853421c2378e487fd61998903
Tim Nguyen <tungnh91@gmail.com>
2017-03-20 20:16:32 -0700

    Change date format and fix layout

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,7 +4,8 @@ import Month from './Month.jsx';
 import $ from 'jquery';
 import t from 'tcomb-form';
 import Button from 'react-bootstrap/lib/Button';
-
+// import Boring from '../../react-client/src/components/boringStuff.js'
+// console.log(Boring.Months)
 class App extends React.Component {
 	constructor(props) {
 		super(props);
@@ -31,12 +32,17 @@ class App extends React.Component {
 		});
 	}
 
-    onSubmit(evt) {
-    evt.preventDefault()
-    const v = this.refs.form.getValue()
-    if (v) {
-      console.log(v)
-    }
+	resetForm() {
+  	this.setState({value: null});
+  }
+
+  onSubmit(evt) {
+  	evt.preventDefault();
+  	this.resetForm();
+  	const v = this.refs.form.getValue()
+  	if (v) {
+    console.log(v)
+  	}
   }
 
 	render() {
@@ -61,9 +67,11 @@ class App extends React.Component {
 
 		const AddTask = AddType.extend({
 		  name: t.Str,
-		  startDate: t.Date,
+		  startDay: Days,
+		  startMonth: Months,
 		  startTime: t.String,
-		  dueDate: t.Date,
+		  dueDay: Days,
+		  dueMonth: Months,
 		  completed: t.Bool,
 		  Prerequesites: t.maybe(t.list(ListOfProjects)),
 		  Dependencies: t.maybe(t.list(ListOfProjects)),
@@ -71,9 +79,11 @@ class App extends React.Component {
 
 		const AddProject = AddType.extend({
 		  name: t.Str,
-		  startDate: t.Date,
+		  startDay: Days,
+		  startMonth: Months,
 		  startTime: t.String,
-		  dueDate: t.Date,
+		  dueDay: Days,
+		  dueMonth: Months,
 		  manager: t.maybe(t.Str),
 		}, 'AddProject')
 
@@ -114,3 +124,54 @@ class App extends React.Component {
 }
 
 export default App;
+
+
+
+const Months = t.enums({
+  1: 'January',
+  2: 'February',
+  3: 'March',
+  4: 'April',
+  5: 'May',
+  6: 'June',
+  7: 'July',
+  8: 'August',
+  9: 'September',
+  10: 'October',
+  11: 'November',
+  12: 'December'
+});
+
+const Days = t.enums({
+	1:1,
+	2:2,
+	3:3,
+	4:4,
+	5:5,
+	6:6,
+	7:7,
+	8:8,
+	9:9,
+	10:10,
+	11:11,
+	12:12,
+	13:13,
+	14:14,
+	15:15,
+	16:16,
+	17:17,
+	18:18,
+	19:19,
+	20:20,
+	21:21,
+	22:22,
+	23:23,
+	24:24,
+	25:25,
+	26:26,
+	27:27,
+	28:28,
+	29:29,
+	30:30,
+	31:31
+})
