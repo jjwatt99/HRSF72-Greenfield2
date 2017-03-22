e955183ddf5112b57fc511c358f0f69c11d8b800
Tim Nguyen <tungnh91@gmail.com>
2017-03-21 20:05:52 -0700

    Pop ups are NOW HAPPENING on all events SON!

--- src/components/Events.jsx
+++ src/components/Events.jsx
@@ -1,12 +1,17 @@
+
 import React from 'react';
+import ShowPopup from './Popup.jsx'
 
 var Events = (props) => {
   return (
-	  <div>
-	    <div>{props.event.date}</div>
-	    <div>{props.event.Name}</div>
-	  </div>
-	);
+    <div>
+      {props.event.date && 
+        <ShowPopup event = {props.event.Name}/>
+      }
+      <button>{props.event.date}</button>
+      <button>{props.event.Name}</button>
+    </div>
+  );
 };
 
 export default Events;
