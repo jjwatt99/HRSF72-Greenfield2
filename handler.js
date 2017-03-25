a9e8d075e7807dfc9be44f18e66ad8b0364bfdd6
dtoney12 <dtoney12@gmail.com>
2017-03-25 07:03:06 -0700

    setup for edit dependencies

--- handler.js
+++ handler.js
@@ -1,5 +1,7 @@
 var dbase = require('./database/db.js');
 var utility = require('./database/utility.js')
+var mongoose = require('mongoose');
+
 
 module.exports = {
 	getUserTasks: function(username, month, callback) {
@@ -16,9 +18,7 @@ module.exports = {
 				callback(sendObj);
 			});
 	},
-	addTask: function(username, taskParams, Prereqs, Depens, month, callback) {
-		// var DependenciesArray = utility.getDependencies(taskParams.brief)
-		// console.log(DependenciesArray);
+	addTask: function(username, taskParams, Prereqs, Depens, month, callback) {	
 		console.log(taskParams.Prerequisites);
 		var newTask = new dbase.Task({
 			Username: username,
@@ -38,22 +38,67 @@ module.exports = {
 	},
 	editTask: function(username, id, month, taskName, startDate, startMonth, startTime, dueDate, dueMonth, completed, Prerequisites, Dependencies, callback ) {
 		
-		dbase.Task.update(
-			{ _id: id }, 
-			{$set: { 	Name: taskName,
-						StartMonth: startMonth,
-						StartDate: startDate,
-						StartTime: startTime,
-						DueMonth: dueMonth,
-						DueDate: dueDate,
-						Prerequisites: Prerequisites,
-						Dependencies: Dependencies,
-						Completed: completed
-				 }}, 
-			function(err) {
+		// var allDependencyIDs = Prerequisites.concat(Dependencies);
+		var EditTaskDeep = function(Prereq, Depen, callback) {
+			var lookupId = mongoose.Types.ObjectId(id.toString());
+			console.log('VVVVVVVV parameter id =', lookupId);
+			dbase.Task.findById( id, function(err, data) {
 				if (err) return console.error(err);
-				module.exports.getUserTasks(username, month, callback);
-			});
+				var startMonthDiff = data.StartMonth - startMonth;
+				var startDateDiff = data.StartDate - startDate;
+				var dueMonthDiff = dueMonth - data.DueMonth;
+				var dueDateDiff  = dueDate - data.DueDate;
+				dbase.Task.update(	{ _id: id }, {$set: { 	Name: taskName,
+															StartMonth: startMonth,
+															StartDate: startDate,
+															StartTime: startTime,
+															DueMonth: dueMonth,
+															DueDate: dueDate,
+															Prerequisites: Prerequisites,
+															Dependencies: Dependencies,
+															Completed: completed }}, 
+					function(err) {
+						if (err) return console.error(err);
+						var index = 0;
+						var upDateAllPrereq = function (Prereq, Depen, index, callback) {
+
+							module.exports.getUserTasks(username, month, callback);
+						}
+						upDateAllPrereq(Prereq, Depen, index, callback);
+					}
+				);
+			})
+		}
+		EditTaskDeep(Prerequisites, Dependencies, callback);
+	},
+	editTask_before: function(username, id, month, taskName, startDate, startMonth, startTime, dueDate, dueMonth, completed, Prerequisites, Dependencies, callback ) {
+		
+		var allDependencyIDs = Prerequisites.concat(Dependencies);
+		var upDateAllDepedencies = function(allDependencyIDs, i) {
+			dbase.Task.update(	{ _id: id }, {$set: { 	Name: taskName,
+														StartMonth: startMonth,
+														StartDate: startDate,
+														StartTime: startTime,
+														DueMonth: dueMonth,
+														DueDate: dueDate,
+														Prerequisites: Prerequisites,
+														Dependencies: Dependencies,
+														Completed: completed }}, 
+				function(err) {
+					if (err) return console.error(err);
+					dbase.Task.update()
+					module.exports.getUserTasks(username, month, callback);
+				});
+		}
+		upDateAllDepedencies();
 	}
 }
+
+
+
+
+
+
+
+
 
